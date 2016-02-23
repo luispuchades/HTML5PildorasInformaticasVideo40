3c6bf52224e2d421c02328c7bb6c9689d089f60d
Luis Puchades <luispuchades@live.com>
2016-02-23 07:52:38 +0100

    HTML5PildorasInformaticasVideo40
    HTML5 - Curso - Píldoras Informáticas - Drag&Drop - Video40 - Drag&Drop
    II - Ejemplo sencillo

--- js/funciones40.js
+++ js/funciones40.js
@@ -11,33 +11,39 @@
 
 //1. Definición de Objetos y Variables
 var elemOrigen;
+var zonaDestino;
 
 elemOrigen = document.getElementById("imagen");
+zonaDestino = document.getElementById("zona-destino");
 
 
 //1.1 Extracción de elementos desde HTML
 
 
 //2. Definición de Funciones
-function comenzar() {
-    elemOrigen.addEventListener("dragstart", function () {
-        alert("Comenzó el evento dragstart");
-    },false)
+function comenzarArrastrar() {
+    
 }
 
-//DRAGEND
-//function comenzar() {
-//    elemOrigen.addEventListener("dragend", function () {
-//        alert("Se activó el evento dragend");
-//    },false)
-//}
 
-//DRAG
-//function comenzar() {
-//    elemOrigen.addEventListener("drag", function () {
-//        alert("Se activó el evento drag");
-//    },false)
-//}
+function comenzar() {
+// Indicamos que en caso de "dragstart" que llame la función comenzarArrastrar.
+    elemOrigen.addEventListener("dragstart",comenzarArrastrar,false);
+
+//Indicamos al navegador que en caso de "dragenter" no haga nada de lo que tiene
+// previsto en su programación para este evento.
+    zonaDestino.addEventListener("dragenter", function(e){
+        e.preventDefault();
+    },false);
+
+//Indicamos al navegador que en caso de "dragover" no haga nada de lo que tiene
+// previsto en su programación para este evento.
+    zonaDestino.addEventListener("dragenter", function(e){
+        e.preventDefault();
+    },false);
+}
+
+
 
 //3. Asignación de Eventos
 document.addEventListener("DOMContentLoaded", comenzar, false);
