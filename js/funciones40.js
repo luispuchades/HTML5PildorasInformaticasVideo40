ef47a8c8c46426ed4d2507b3b0cd68d672579f34
Luis Puchades <luispuchades@live.com>
2016-02-24 07:03:52 +0100

    HTML5PildorasInformaticasVideo40
    HTML5 - Curso - Píldoras Informáticas - Drag&Drop - Video40 - Ejemplo
    Sencillo

--- js/funciones40.js
+++ js/funciones40.js
@@ -21,8 +21,34 @@ zonaDestino = document.getElementById("zona-destino");
 
 
 //2. Definición de Funciones
-function comenzarArrastrar() {
-    
+
+//Creamos la función comenzarArrastrar y le pasamos el objeto "e"
+//que desencadena el evento "dragstart".
+function comenzarArrastrar(e) {
+    var ruta;
+    var codigo;
+
+//Asignamos a la variable ruta el atributo src de la imagen para que tenga
+//la ruta automáticamente
+    ruta = elemOrigen.getAttribute("src");
+    codigo = "<img src='" + ruta + "'>";
+
+//Asignamos al método setData de la propiedad dataTransfer perteneciente al objeto
+// "e" del evento desencadenado el formato "Text" a transferir y la ruta completa 
+//que en este caso es la variable "codigo"
+    e.dataTransfer.setData("Text", codigo);
+}
+
+
+//Definimos la función "soltado" para cuando hagamos "drop"
+function soltado(e) {
+//Reseteamos el comportamiento por defecto del navegador para "drop" del objeto
+//"e" desencadenado por el evento "dragstart"
+    e.preventDefault();
+//Le indicamos que cree/asigne el código HTML que rescatamos con el método
+//getdata correspondiente a los datos que habíamos indicado que ibamos a
+//transferir con setData en la función anterior.
+    zonaDestino.innerHTML = e.dataTransfer.getData("Text");
 }
 
 
@@ -38,9 +64,15 @@ function comenzar() {
 
 //Indicamos al navegador que en caso de "dragover" no haga nada de lo que tiene
 // previsto en su programación para este evento.
-    zonaDestino.addEventListener("dragenter", function(e){
+    zonaDestino.addEventListener("dragover", function(e){
         e.preventDefault();
     },false);
+
+//Indicamos al navegador que en caso de "dragover" no haga nada de lo que tiene
+// previsto en su programación para este evento.
+    zonaDestino.addEventListener("drop", soltado,false);
+
+
 }
 
 
